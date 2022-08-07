199a7844b81b07c5fe36aba393c2e1824c43d01b
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2022-08-07 16:39:36 +0200

    Update reproducibility test

--- test/test_reproducibility.py
+++ test/test_reproducibility.py
@@ -1,6 +1,5 @@
 import unittest
 import os
-import shutil
 import sys
 from pathlib import Path
 import filecmp
@@ -12,9 +11,7 @@ import run
 
 class TestRuns(unittest.TestCase):
     def test_runs_are_reproducible(self):
-        outputs_path = (Path(__file__).parent / 'outputs').absolute()
-        if os.path.exists(outputs_path):
-            shutil.rmtree(outputs_path)
+        outputs_path = Path(os.getcwd()) / 'outputs'
         seed = 42
         sys.argv = []
         sys.argv.extend([
@@ -25,13 +22,16 @@ class TestRuns(unittest.TestCase):
             'run.train_steps=10',
             'run.eval_freq=10',
             f'+run.seed={seed}',
+            'tag=_reproducibility-test',
         ])
         run.main()
         run.main()
         history_files = glob.glob(f'{outputs_path}/**/env_history.json', recursive=True)
         progress_files = glob.glob(f'{outputs_path}/**/progress.json', recursive=True)
-        history1, history2 = history_files
-        progress1, progress2 = progress_files
+        history_files.sort()
+        progress_files.sort()
+        history1, history2 = history_files[-2:]
+        progress1, progress2 = progress_files[-2:]
         self.assertTrue(filecmp.cmp(history1, history2))
         self.assertTrue(filecmp.cmp(progress1, progress2))
 
