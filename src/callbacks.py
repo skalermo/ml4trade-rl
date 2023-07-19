9e54afff006f6858cba12ed1f8fbe4f5874621fb
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-07-19 11:07:56 +0200

    Resample on rollout end

--- src/callbacks.py
+++ src/callbacks.py
@@ -3,8 +3,7 @@ from stable_baselines3.common.callbacks import BaseCallback
 
 class ResampleCallback(BaseCallback):
     def _on_step(self) -> bool:
-        self.model.policy.resample()
         return True
 
-    def _on_rollout_start(self) -> None:
+    def _on_rollout_end(self) -> None:
         self.model.policy.resample()
