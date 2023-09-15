5887b78540897c835190448d7ddcdc33dc1245d7
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-09-15 15:31:49 +0200

    Remove MultiHeadPolicy

--- src/custom_policy.py
+++ src/custom_policy.py
@@ -1,9 +1,7 @@
-from typing import Callable, Tuple, Optional
+from typing import Callable, Tuple
 
-import numpy as np
 from gymnasium import spaces
 import torch as th
-from stable_baselines3.common.distributions import DiagGaussianDistribution
 from stable_baselines3.common.type_aliases import Schedule
 from torch import nn
 from stable_baselines3.common.policies import ActorCriticPolicy
@@ -99,95 +97,3 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
         for module in (self.action_net, self.value_net):
             if isinstance(module, NoisyLinear):
                 module.resample()
-
-
-class CustomMultiHeadPolicy(CustomActorCriticPolicy):
-    def _build(self, lr_schedule: Schedule) -> None:
-        super(CustomActorCriticPolicy, self)._build(lr_schedule)
-        self.action_net = CustomMultiHead(self.action_net.in_features)
-
-    def forward(self, obs: th.Tensor, deterministic: bool = False) -> Tuple[th.Tensor, th.Tensor, th.Tensor]:
-        features = self.extract_features(obs)
-        if self.share_features_extractor:
-            latent_pi, latent_vf = self.mlp_extractor(features)
-        else:
-            pi_features, vf_features = features
-            latent_pi = self.mlp_extractor.forward_actor(pi_features)
-            latent_vf = self.mlp_extractor.forward_critic(vf_features)
-        # Evaluate the values for the given observations
-        values = self.value_net(latent_vf)
-
-        distribution = []
-        for h in self.action_net.heads:
-            mean_actions = h(latent_pi)
-            action_dist = DiagGaussianDistribution(1)
-            distribution.append(
-                action_dist.proba_distribution(mean_actions, th.tensor([0] * 1))
-            )
-
-        actions = th.stack([d.get_actions(deterministic=deterministic) for d in distribution])
-        if not deterministic:
-            mask = np.random.random((96, obs.shape[0]))
-            threshold = 0.20
-            mask[mask < threshold] = 0
-            mask[mask >= threshold] = 1
-            mask = th.tensor(mask)
-            actions[mask == 0] = th.tensor([-20.0] * 1)
-
-        log_probs = []
-        for i, d in enumerate(distribution):
-            log_probs.append(
-                d.log_prob(actions[i, :, :])
-            )
-
-        log_probs = th.stack(log_probs)
-        if not deterministic:
-            log_probs[mask == 0] = 0.0
-        log_prob = th.sum(log_probs, dim=0)
-
-        actions = th.transpose(actions, 0, 1)
-        actions = actions.reshape((-1, *self.action_space.shape))
-        return actions, values, log_prob
-
-    def evaluate_actions(self, obs: th.Tensor, actions: th.Tensor) -> Tuple[th.Tensor, th.Tensor, Optional[th.Tensor]]:
-        features = self.extract_features(obs)
-        if self.share_features_extractor:
-            latent_pi, latent_vf = self.mlp_extractor(features)
-        else:
-            pi_features, vf_features = features
-            latent_pi = self.mlp_extractor.forward_actor(pi_features)
-            latent_vf = self.mlp_extractor.forward_critic(vf_features)
-        distribution = []
-        for h in self.action_net.heads:
-            mean_actions = h(latent_pi)
-            action_dist = DiagGaussianDistribution(1)
-            distribution.append(
-                action_dist.proba_distribution(mean_actions, th.tensor([0] * 1))
-            )
-
-        actions = th.stack(actions.split(1, dim=1))
-        mask = (actions > -20)[:, :, 0]
-
-        log_probs = []
-        for i, d in enumerate(distribution):
-            log_probs.append(
-                d.log_prob(actions[i])
-            )
-        log_probs = th.stack(log_probs)
-        log_probs[mask == 0] = 0.0
-        log_prob = th.sum(log_probs, dim=0)
-
-        values = self.value_net(latent_vf)
-        entropy = th.sum(th.stack([d.entropy() for d in distribution]))
-        return values, log_prob, entropy
-
-
-class CustomMultiHead(nn.Module):
-    def __init__(self, in_features: int):
-        super().__init__()
-
-        self.heads = [nn.Linear(in_features, 1) for _ in range(96)]
-
-    def forward(self, x):
-        outs = [h(x) for h in self.heads]
-        return th.concat(outs, dim=1)
