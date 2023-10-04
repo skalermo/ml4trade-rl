3c20ba4dedbf6f918fbc46b99f3c22876c26abf7
Roman Moskalenko <34585360+skalermo@users.noreply.github.com>
2023-10-05 00:22:42 +0200

    Add random agent

---
+++ src/random_agent.py
@@ -0,0 +1,15 @@
+import gymnasium
+
+
+class RandomAgent:
+    def __init__(self, env: gymnasium.Env, *args, **kwargs):
+        self.env = env
+        self.action_space = env.action_space
+
+    def learn(self, total_timesteps: int, *args, **kwargs):
+        self.env.reset()
+        for _ in range(total_timesteps):
+            self.env.step(self.action_space.sample())
+
+    def predict(self, *args, **kwargs):
+        return self.action_space.sample(), None
